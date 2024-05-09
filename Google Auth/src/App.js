187a9d715baeebc354feea715a74e69f29bd0308
parmar rachit <135450523+sturachit@users.noreply.github.com>
2024-05-09 14:48:27 +0530

    Add files via upload

---
+++ Google Auth/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { GoogleLogin } from '@react-oauth/google';
+// import Googledemo from './Component/ReactLogin';
+import ReactLogin from './Component/ReactLogin';
+
+function App() {
+  return(
+  <ReactLogin/>
+  )
+}
+export default App;
