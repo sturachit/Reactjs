2276c8d9a11124cb46fc8cb27a71ee9cb83650bd
parmar rachit <135450523+sturachit@users.noreply.github.com>
2024-05-09 14:18:35 +0530

    Add files via upload

---
+++ Project 3/material_ui/src/App.js
@@ -0,0 +1,16 @@
+// import logo from '../src/images/logo.svg';
+import './App.css';
+import Dashboard from './Component/Dashboard';
+import Footer from './Component/Footer'
+function App() {
+  return (
+    <div className="App">
+       
+      <Dashboard/>
+      
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
