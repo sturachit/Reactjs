cd01642830daed7912bbfb623767b880749e1073
parmar rachit <135450523+sturachit@users.noreply.github.com>
2024-05-09 14:15:42 +0530

    Add files via upload

---
+++ Project 1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ArrayQue from './Components/ArrayQue';
+
+function App() {
+  return (
+    <div className="App">
+      <ArrayQue />
+      
+    </div>
+  );
+}
+
+export default App;
